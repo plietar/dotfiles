94989ee9d548555b657126438309d18a10ae9f44
Paul Lietar <paul@lietar.net>
2013-03-25 19:11:24 +0100

    Add hombrew to PATH, when available

---
+++ zsh/homebrew.zsh
@@ -0,0 +1,18 @@
+if [[ -d "$HOMEBREW_ROOT" ]]; then
+    export PATH="$HOMEBREW_ROOT/bin":$PATH
+
+    if [[ -d "$HOMEBREW_ROOT/opt/coreutils/libexec/gnubin" ]]; then
+        export PATH="$HOMEBREW_ROOT/opt/coreutils/libexec/gnubin":$PATH
+    fi
+    if [[ -d "$HOMEBREW_ROOT/opt/coreutils/libexec/gnuman" ]]; then
+        export MANPATH="$HOMEBREW_ROOT/opt/coreutils/libexec/gnuman":$MANPATH
+    fi
+
+    if [[ -d "$HOMEBREW_ROOT/opt/findutils" ]]; then
+        alias find=gfind
+        alias locate=glocate
+        alias updatedb=gupdatedb
+        alias xargs=gxargs
+        alias oldfind=goldfind
+    fi
+fi
