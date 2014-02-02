87247b6d734ab536ddda4576744509458d7ecc04
Paul Lietar <paul@lietar.net>
2014-02-02 12:49:58 +0000

    Use zsh's vi-mode.

--- zsh/00_oh-my-zsh.zsh
+++ zsh/00_oh-my-zsh.zsh
@@ -25,7 +25,7 @@ ZSH_THEME="agnoster"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git git-extras git-flow bzr rbenv brew vagrant)
+plugins=(git git-extras git-flow bzr rbenv brew vagrant osx vi-mode)
 
 source $ZSH/oh-my-zsh.sh
 
